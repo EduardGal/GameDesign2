628fc698c091ac0ab3090078d5de9e435756bced
niknorine <nikitanorine@gmail.com>
2019-03-25 00:44:21 +0000

    Can teleport to tree house

---
+++ Advanced Games Design/Assets/enterTreeHouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enterTreeHouse : MonoBehaviour
+{
+
+    public Transform treePos;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "PlayerOne" || other.tag == "PlayerTwo")
+        {
+            if (Input.GetKeyDown(KeyCode.E)){
+                other.gameObject.transform.position = treePos.position;
+            }
+        }
+    }
+}
