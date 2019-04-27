81819ebd1e2388d5c0640d0f7b7167e80a1a807e
Eduard <eg9706j@greenwich.ac.uk>
2019-04-27 20:52:24 +0100

    Town update
    + vegetration
    + mountains and volcano
    + treehouse blockage and exit function

---
+++ Advanced Games Design/Assets/exitTreeHouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitTreeHouse : MonoBehaviour
+{
+
+    public Transform groundPos;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PlayerOne" || other.tag == "PlayerTwo")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                other.gameObject.transform.position = groundPos.position;
+            }
+        }
+    }
+}
