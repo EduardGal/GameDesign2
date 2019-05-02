0096c89c7e3d52fa91acd5399199132631bf078b
tudordrobota <td4681r@greenwich.ac.uk>
2019-05-02 11:47:09 +0100

    Edit
    edit

--- Advanced Games Design/Assets/Scripts/enterTreeHouse.cs
+++ Advanced Games Design/Assets/Scripts/enterTreeHouse.cs
@@ -6,12 +6,8 @@ public class enterTreeHouse : MonoBehaviour
 {
 
     public Transform treePos;
-    public GameObject gameObject;
 
-    public void DeactivateMe()
-    {
-        StartCoroutine(RemoveAfterSeconds(3));
-    }
+
 
 
     private void OnTriggerStay(Collider other)
@@ -20,17 +16,9 @@ public class enterTreeHouse : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E)){
                 other.gameObject.transform.position = treePos.position;
-                gameObject.SetActive(true);
 
             }
 
         }
-    }
-
-
-    IEnumerator RemoveAfterSeconds(int seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-        gameObject.SetActive(false);
     }
 }
