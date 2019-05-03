a75a29cfbaec23fb98a0829ebac331dfef209330
tudordrobota <td4681r@greenwich.ac.uk>
2019-05-04 00:49:59 +0100

    Revert "audio"
    This reverts commit b9c590dd2b079e8a361b7a8f5496805d3635994c, reversing
    changes made to 1d2f7d2e4bf27d5ff32d95edddc8fbbb8d5e01d9.

--- Advanced Games Design/Assets/Scripts/Items/ItemPickup.cs
+++ Advanced Games Design/Assets/Scripts/Items/ItemPickup.cs
@@ -43,6 +43,7 @@ public class ItemPickup : Interactables
         {
             Destroy(gameObject);
             pickupPanel.SetActive(false);
+            FindObjectOfType<AudioManager>().Play("Item");
         }
     }
 
---
+++ Advanced Games Design/Assets/Scripts/UI/TrigBush.cs
@@ -0,0 +1,26 @@
+﻿ using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+ public class TrigBush : MonoBehaviour
+{
+	 
+     // Use this for initialization
+     void Start () { 
+		 
+     }
+     
+     void OnTriggerEnter (Collider player)
+     {
+		 if(player.gameObject.tag == "PlayerOne")
+		 {
+			 
+			 FindObjectOfType<AudioManager>().Play("Bush");
+     }
+ 
+    else if (player.gameObject.tag == "PlayerTwo")
+        {
+            FindObjectOfType<AudioManager>().Play("Bush");
+        }
+}
+}
+	 
