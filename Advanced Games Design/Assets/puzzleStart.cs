85d303462b1527230ca40b8151378ee0abbd9118
niknorine <nikitanorine@gmail.com>
2019-05-03 23:28:30 +0100

    moved files into folders

---
+++ Advanced Games Design/Assets/puzzleStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzleStart : MonoBehaviour
+{
+    public GameObject puzzleCanvas;
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "PlayerOne" || other.tag == "PlayerTwo")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                puzzleCanvas.SetActive(true);
+                other.GetComponent<Camera>().enabled = false;
+
+            }
+        }
+    }
+}
