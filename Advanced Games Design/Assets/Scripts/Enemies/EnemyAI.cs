3e548082e524102904f3973d61650e5f18dfffb9
tudordrobota <td4681r@greenwich.ac.uk>
2019-05-04 21:09:46 +0100

    Revert "Sounds"
    This reverts commit a8aec9a240479f68c6b3da446d1f71a1ebaf1358, reversing
    changes made to 69568b479ee576fc7de72108465772b175a2df7a.

---
+++ Advanced Games Design/Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+    [SerializeField] float patrolSpeed = 1.0f;
+    [SerializeField] float patrolWaitTime = 1.0f;
+    [SerializeField] float chaseSpeed = 1.5f;
+    [SerializeField] float chaseWaitTime = 5.0f;
+    [SerializeField] float stoppingDistanceFromPlayers = 7.0f;
+    [SerializeField] Transform[] patrolWayPoints;
+
+    private EnemySenses enemySenses;
+    private NavMeshAgent navMeshAgent;
+    private Transform playerOne;
+    private Transform playerTwo;
+    private PlayersLastLocation playersLastLocation;
+
+    private float patrolTimer;
+    private float chaseTimer;
+    private int currentWaypointIndex;
+
+    private void Awake()
+    {
+        enemySenses = GetComponent<EnemySenses>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        playerOne = GameObject.FindGameObjectWithTag("PlayerOne").transform;
+        playerTwo = GameObject.FindGameObjectWithTag("PlayerTwo").transform;
+        playersLastLocation = GameObject.FindGameObjectWithTag("GameController").GetComponent<PlayersLastLocation>();
+    }
+
+    private void Update()
+    {
+        if (enemySenses.isPlayerOneInSight || enemySenses.isPlayerTwoInSight)
+        {
+            AnalysePlayer();
+        }
+        else if(enemySenses.playerOneLastKnownLocation != playersLastLocation.resetPosition || enemySenses.playerTwoLastKnownLocation != playersLastLocation.resetPosition)
+        {
+            ChasePlayer();
+        }
+        else
+        {
+            PatrolWaypoints();
+        }
+            
+    }
+
+    void AnalysePlayer()
+    {
+        // EnemyAnalyse script should trigger - if the player runs out of range, the enemy should run towards their last known location before going back to patrolling
+        
+    }
+
+    void ChasePlayer()
+    {
+        Vector3 distanceToPlayerOne = enemySenses.playerOneLastKnownLocation - transform.position;
+        Vector3 distanceToPlayerTwo = enemySenses.playerTwoLastKnownLocation - transform.position;
+
+        //TODO: ensure that the enemy runs towards the closest player to their current location
+        if (distanceToPlayerOne.sqrMagnitude > 4.0f)
+        {
+            navMeshAgent.destination = enemySenses.playerOneLastKnownLocation;
+            navMeshAgent.stoppingDistance = stoppingDistanceFromPlayers;
+
+        }
+        else if (distanceToPlayerTwo.sqrMagnitude > 4.0f)
+        {
+            navMeshAgent.destination = enemySenses.playerTwoLastKnownLocation;
+            navMeshAgent.stoppingDistance = stoppingDistanceFromPlayers;
+        }
+
+        //TODO: tinker with the chaseSpeed - perhaps slow the speed when the enemy is analysing the player to allow the player to escape in time
+        navMeshAgent.speed = chaseSpeed;
+
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            chaseTimer += Time.deltaTime;
+
+            if(chaseTimer > chaseWaitTime)
+            {
+                playersLastLocation.playerOnePosition = playersLastLocation.resetPosition;
+                playersLastLocation.playerTwoPosition = playersLastLocation.resetPosition;
+                enemySenses.playerOneLastKnownLocation = playersLastLocation.resetPosition;
+                enemySenses.playerTwoLastKnownLocation = playersLastLocation.resetPosition;
+                chaseTimer = 0.0f;
+            }
+        }
+        else
+        {
+            chaseTimer = 0.0f;
+        }
+
+    }
+    //If the player escapes the enemy, they should continue to patrol
+    void PatrolWaypoints()
+    {
+        navMeshAgent.speed = patrolSpeed;
+
+        if(navMeshAgent.destination == playersLastLocation.resetPosition || navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            patrolTimer += Time.deltaTime;
+
+            if(patrolTimer >= patrolWaitTime)
+            {
+                if (currentWaypointIndex == patrolWayPoints.Length - 1)
+                    currentWaypointIndex = 0;
+                else
+                    currentWaypointIndex++;
+
+                patrolTimer = 0.0f;
+            }
+        }
+        else
+        {
+            patrolTimer = 0.0f;
+        }
+
+        navMeshAgent.destination = patrolWayPoints[currentWaypointIndex].position;
+        navMeshAgent.baseOffset = 0.25f;
+    }
+
+}
---
+++ Advanced Games Design/Assets/Scripts/Player/RockTarget.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTarget : MonoBehaviour
+{
+    public Material enemiesDefaultMaterial;
+    [SerializeField] Material silhoutteMaterial;
+
+    Renderer renderer;
+
+    SphereCollider sphere;
+    Collider newCollider;
+
+    private bool trigger;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sphere = GetComponent<SphereCollider>();
+    }
+
+    private void Update()
+    {
+        if (trigger && sphere != null)
+        {
+            if (newCollider.gameObject.tag == "Enemy")
+            {
+                var enemyBody = newCollider.transform.Find("Body_low");
+
+                Debug.Log("Body found");
+
+                renderer = enemyBody.gameObject.GetComponent<Renderer>();
+                renderer.sharedMaterial = silhoutteMaterial;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        newCollider = other;
+        trigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        newCollider = null;
+        trigger = false;
+
+        if (other.gameObject.tag == "Enemy")
+        {
+
+            Debug.Log("I am an enemy");
+
+            var enemyBody = other.transform.Find("Body_low");
+            renderer = enemyBody.gameObject.GetComponent<Renderer>();
+            renderer.sharedMaterial = enemiesDefaultMaterial;
+        }
+    }
+}
