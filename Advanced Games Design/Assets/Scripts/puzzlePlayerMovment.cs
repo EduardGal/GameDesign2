804ae3ce3361370163699b93d4fdbd4f37299e94
niknorine <nikitanorine@gmail.com>
2019-05-04 14:07:13 +0100

    Puzzle updates

---
+++ Advanced Games Design/Assets/Scripts/puzzlePlayerMovment.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzlePlayerMovment : Photon.MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    public bool devTesting;
+
+    public GameObject triggerObj;
+
+
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        gameObject.GetComponent<TrailRenderer>().sortingLayerName = ("Defualt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(gameObject.GetComponent<Rigidbody2D>().velocity != new Vector2(0, 0))
+        {
+            Instantiate(triggerObj, this.gameObject.transform.position, Quaternion.identity);
+        }
+        CheckInput();
+        
+        
+    }
+
+    private void CheckInput()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        body.velocity = new Vector2(horizontal * 15, vertical * 15);
+    }
+
+
+
+
+}
