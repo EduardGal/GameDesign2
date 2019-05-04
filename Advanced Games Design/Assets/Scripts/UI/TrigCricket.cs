5e50c74f6cdc7f524f821386babbf4586323e2f0
tudordrobota <td4681r@greenwich.ac.uk>
2019-05-04 16:02:23 +0100

    cricket sound

---
+++ Advanced Games Design/Assets/Scripts/UI/TrigCricket.cs
@@ -0,0 +1,26 @@
+﻿ using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+ public class TrigCricket : MonoBehaviour
+{
+	 
+     // Use this for initialization
+     void Start () { 
+		 
+     }
+     
+     void OnTriggerEnter (Collider player)
+     {
+		 if(player.gameObject.tag == "PlayerOne")
+		 {
+			 
+			 FindObjectOfType<AudioManager>().Play("Cricket");
+     }
+ 
+    else if (player.gameObject.tag == "PlayerTwo")
+        {
+            FindObjectOfType<AudioManager>().Play("Cricket");
+        }
+}
+}
+	 
