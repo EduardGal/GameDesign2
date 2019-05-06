bc87ce0fa281ea03b8583a02f04e20e000e053ea
niknorine <nikitanorine@gmail.com>
2019-05-06 17:58:24 +0100

    kick player implemented

---
+++ Advanced Games Design/Assets/kickPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kickPlayer : Photon.MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (PhotonNetwork.player.IsMasterClient)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+
+    public void OnKickPlayer()
+    {
+      
+
+            PhotonNetwork.CloseConnection(PhotonPlayer.Find(2));
+
+       
+    }
+
+}
